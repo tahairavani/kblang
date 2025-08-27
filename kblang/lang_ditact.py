0ec2c7927acd6790c4e5c38fd87567326470d593
alisaadati10 <allisaadati290@gmail.com>
2025-08-27 12:02:08 +0430

    I made the changes I was supposed to make in lang_ditect

--- kblang/lang_ditact.py
+++ kblang/lang_ditact.py
@@ -1,11 +1,17 @@
 from layouts.keyboard_layouts import KeyboardLayout
 from layouts.persian import FaLayout
 from layouts.english import EnLayout
+from converter import ConvertLang
 
 class Langueageditector:
-    langueages = KeyboardLayout.get_keyboard_layouts()
     def __init__(self, text):
         self.text = text 
     def get_langueage(self):
-        return(self.text)
-print(Langueageditector("hello").get_langueage())
+        lang = KeyboardLayout.get_keyboard_layouts()
+        for t in self.text:
+           for key, value in lang:
+              if key in lang:
+                return KeyboardLayout.language_code
+detector =  Langueageditector("سلام")
+print(detector.get_langueage())
+print(KeyboardLayout.language_code)
