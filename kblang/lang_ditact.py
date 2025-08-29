b39cb90a475a4dec346ff12a2e228fed5d8eabfe
alisaadati10 <allisaadati290@gmail.com>
2025-08-29 14:15:53 +0430

    This issue was resolved.

--- kblang/lang_ditact.py
+++ kblang/lang_ditact.py
@@ -5,13 +5,26 @@ from converter import ConvertLang
 
 class Langueageditector:
     def __init__(self, text):
-        self.text = text 
-    def get_langueage(self):
-        lang = KeyboardLayout.get_keyboard_layouts()
-        for t in self.text:
-           for key, value in lang:
-              if key in lang:
-                return KeyboardLayout.language_code
-detector =  Langueageditector("سلام")
-print(detector.get_langueage())
-print(KeyboardLayout.language_code)
+        self.text  = text
+    def get_languages(self):
+        scores = {}
+        total_charts = 0
+
+        for ch in self.text.lower():
+            for lang, alphabet in KeyboardLayout.get_keyboard_layouts().items():
+                if ch in alphabet:
+                    scores[lang] = scores.get(lang, 0) + 1
+                    total_charts += 1
+
+    #mohasebe darsad
+        percentages = {}
+        for lang, count in scores.items():
+            percentages[lang] = round((count / total_charts) * 100, 2) if total_charts > 0 else 0 
+
+        #peyda kardan zaban asli
+        best_lang = max(percentages, key=percentages.get) if percentages else None
+        return best_lang, percentages  
+           
+
+detect = Langueageditector("hello سلام")
+print(detect.get_languages())
