8ad805a9c7bc9529a3678e9820f856bdabcd556c
Rajesh Agrawal <Rajesh.Agarawal@gmail.com>
2025-09-04 15:55:01 +0530

    Update the KeyboardLanguageFixer class.

--- kblang/lang_fixer.py
+++ kblang/lang_fixer.py
@@ -1,13 +1,16 @@
-from kblang import kbfix
 from wordfreq import word_frequency
+from .converter import ConvertLang
+from .layouts.keyboard_layouts import KeyboardLayout
+from .layouts.load_keyboard_layouts import load_layouts
 
 class KeyboardLanguageFixer:
-    def __init__(self, candidates=None, min_freq=1e-6):
+    def __init__(self, min_freq=1e-6):
         """
-        :param candidates: list of language codes to check (default: ['fa', 'ar', 'ru'])
         :param min_freq: threshold for word validity using wordfreq
         """
-        self.candidates = candidates or ["fa", "ar", "ru"]
+        load_layouts()
+        # get all available layouts dynamically
+        self.available_languages = list(KeyboardLayout.get_keyboard_layouts().keys())
         self.min_freq = min_freq
 
     def score_text(self, text, lang):
@@ -15,41 +18,24 @@ class KeyboardLanguageFixer:
         Score a corrected text by counting valid dictionary words
         """
         words = text.split()
-        score = 0
-        for w in words:
-            if word_frequency(w, lang) > self.min_freq:
-                score += 1
-        return score
+        return sum(1 for w in words if word_frequency(w, lang) > self.min_freq)
 
     def fix_text(self, text):
         """
-        It will try all languages and pick the best correction.
-        
-        To use in client code follow below way:-
-
-        fixer = KeyboardLanguageFixer()
-        print(fixer.fix_text("sghl ]x,vd lk o,fl"))
-
-        Output:
-         {
-           "original": "sghl ]x,vd lk o,fl",
-           "corrected": "سلام خوبم چطوری",
-           "language": "fa",
-           "is_gibberish": False
-         }
+        Try all available layouts and pick the best correction.
         """
         best_lang = None
         best_score = 0
         best_text = None
 
-        for lang in self.candidates:
-            corrected = kbfix(text, lang)
-            score = self.score_text(corrected, lang)
+        for lang in self.available_languages:
+            converted = ConvertLang(text, to_lang=lang).get_converted_text()
+            score = self.score_text(converted, lang)
 
             if score > best_score:
                 best_score = score
                 best_lang = lang
-                best_text = corrected
+                best_text = converted
 
         return {
             "original": text,
